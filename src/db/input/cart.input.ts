0fa1ce32855c0bb17b964590ff1388364e3e0b91
hoanglong040800 <hoanglong040800@gmail.com>
2024-10-21 21:34:57 +0700

    fix: cart type

--- src/db/input/cart.input.ts
+++ src/db/input/cart.input.ts
@@ -3,13 +3,24 @@ import {
   IsNotEmpty,
   IsNumber,
   IsOptional,
+  IsString,
   IsUUID,
   Validate,
   ValidateNested,
 } from 'class-validator';
-import { ICartItem } from '../interface';
+import { ICartItem, IProduct } from '../interface';
 import { IsNotNegative } from '@/modules/_base';
 import { Type } from 'class-transformer';
+
+export class MutateCartItemProduct implements IProduct {
+  @IsOptional()
+  @IsUUID()
+  id?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
 
 export class MutateCartItem implements ICartItem {
   @IsOptional()
@@ -24,6 +35,11 @@ export class MutateCartItem implements ICartItem {
   @IsUUID()
   @IsNotEmpty()
   productId: string;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => MutateCartItemProduct)
+  product?: MutateCartItemProduct;
 }
 
 export class AddCartItemBody {
