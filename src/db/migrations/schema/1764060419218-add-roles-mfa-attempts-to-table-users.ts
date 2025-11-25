7abf6a74cc2d87e4318123a4b3e26c78ebf8a173
hoanglong040800 <hoanglong040800@gmail.com>
2025-11-25 16:22:04 +0700

    RSP-114 feat: migrate users

---
+++ src/db/migrations/schema/1764060419218-add-roles-mfa-attempts-to-table-users.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddRolesMfaAttemptsToTableUsers1764060419218
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.query(`
+        CREATE TYPE user_role_enum as ENUM ('admin', 'shopper');
+      `);
+
+      await queryRunner.query(`
+        ALTER TABLE users
+        ADD COLUMN roles user_role_enum NOT NULL DEFAULT 'shopper',
+        ADD COLUMN otp_code varchar(20),
+        ADD COLUMN login_attempts smallint,
+        ADD COLUMN mfa_sent_at timestamp;
+      `);
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE users
+            DROP COLUMN roles,
+            DROP COLUMN otp_code,
+            DROP COLUMN login_attempts,
+            DROP COLUMN mfa_sent_at;
+        `);
+
+    await queryRunner.query(`
+            DROP TYPE user_role_enum;
+        `);
+  }
+}
